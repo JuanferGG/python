dd7671f7bc9a7665f0886bd784d69331dfb7c0ef
JuanferGG <juanfe15777@gmail.com>
2025-07-24 15:04:17 -0500

    Add initial implementation of list creation examples

---
+++ 02_flow_control/03_list.py
@@ -0,0 +1,16 @@
+###
+#? 03 - Listas
+#? Secuencias mutables de elementos.
+#? Pueden contener elementos de diferentes tipos.
+###
+
+
+from os import system
+if system("clear") != 0: system("cls")
+
+# Creación de listas
+print("\nCrear listas")
+lista1 = [1, 2, 3, 4, 5] # lista de enteros
+lista2 = ["manzanas", "peras", "plátanos"] # lista de cadenas
+lista3 = [1, "hola", 3.14, True] # lista de tipos mixtos
+
