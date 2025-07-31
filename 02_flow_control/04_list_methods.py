373059fb40e9536979f914f9db3a32948ed717bc
JuanferGG <juanfe15777@gmail.com>
2025-07-31 18:04:54 -0500

    Add additional list methods and examples for sorting and counting elements

--- 02_flow_control/04_list_methods.py
+++ 02_flow_control/04_list_methods.py
@@ -30,3 +30,42 @@ print("Lista actualizada:", lista1)
 
 lista1.pop(1) #! --> basicamente recibe el indice valor por defecto: -1 el ultimo
 print("Lista con índice borrado:", lista1)
+
+#! Eliminar por lo bestia
+del lista1[-1]
+print(lista1)
+
+lista1.clear() #* Elimina todos los elementos de la lista
+print(lista1)
+
+#! ELiminar un rango de elementos
+lista2 = ['🐼', '🐨', '🐶', '😿', '🐹']
+del lista2[1:3]
+print(lista2)
+
+#! mas metodos utiles
+print('Ordenar listas modificando la original')
+numbers = [3, 10, 2, 8, 99, 101]
+numbers.sort()
+print('Modificando lista:', numbers)
+
+print('Ordenar listas creando una nueva')
+numbers = [3, 10, 2, 8, 99, 101]
+sorted_numbers = sorted(numbers)
+print('Lista nueva:', sorted_numbers)
+
+print("Ordenar una lista de cadenas de texto (mezclas mayúscula y minúscula)")
+frutas = ['manzana', 'pera', 'limón', 'manzana', 'pera', 'limón']
+sorted_frutas = sorted(frutas)
+print('Frutas minusculas ordenadas:', sorted_frutas)
+
+frutas = ['manzana', 'Pera', 'Limón', 'manzana', 'pera', 'limón']
+frutas.sort(key=str.lower)
+print(frutas)
+
+#! Más cositas útiles
+animals = ['🐶', '🐼', '🐨', '🐶']
+print('Tamaño de lista', len(animals)) #? Tamaño de la lista
+print('Cantidad de veces repetido', animals.count('🐶')) #? Cuantas veces aparece un elemento
+print('🐼' in animals) #? Comprueba si hay un '🐼' en la lista -> True
+
