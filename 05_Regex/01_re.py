9425cc45d152f159b98545c86c6a9d252f510efe
JuanferGG <juanfe15777@gmail.com>
2025-09-02 18:00:42 -0500

    Regex

---
+++ 05_Regex/01_re.py
@@ -0,0 +1,16 @@
+##
+# 01 - Expresiones regulares
+# 
+
+""" Las expresiones regulares son una secuencia de caracteres que forman un patrón de búsqueda.
+    Se utilizan para la búsqueda de cadenas de texto, validación de datos, etc. """
+
+
+""" ¿Por qué aprender Regex?
+
+- Búsqueda avanzada: Encontrar patrones específicos en textos grandes de forma rápida y precisa. (un editor de Markdown sólo usando Regex)
+
+- Validación de datos: Asegurarte que los datos que ingresa un usuario como el email, teléfono, etc. son correctos.
+
+- Manipulación del texto: Extraer, reemplazar y modificar partes de la cadena de texto fácilmente
+"""
