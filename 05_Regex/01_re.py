030e3c8ddc44832f87e17429fbf9e9d4912b32b4
JuanferGG <juanfe15777@gmail.com>
2025-09-06 08:54:35 -0500

    Add regex examples and exercises for pattern matching and validation

--- 05_Regex/01_re.py
+++ 05_Regex/01_re.py
@@ -14,3 +14,45 @@
 
 - Manipulación del texto: Extraer, reemplazar y modificar partes de la cadena de texto fácilmente
 """
+
+from os import system
+if system("clear") != 0: system("cls")
+
+#! 1. Importar el módulo de expresiones regulares "re"
+import re
+#? 2. Crear un patrón, que es una cadena de texto que describe lo que queremos encontrar
+pattern = "Hola"
+#? 3. El texto donde queremos buscar
+text = "Hola mundo"
+#? 4. Usar la función de búsqueda de "re"
+result = re.search(pattern, text)
+
+if result:
+    print(f"El resultado es: {result}")
+else:
+    print("No he encontrado patron en el texto")
+
+
+#! .group() devuelve la cadena que coincide con el pattern
+print(result.group())
+
+#! .start() devolver la posición inicial de la coincidencia
+print(result.start())
+
+#! .end() devolver la posición final de la coincidencia
+print(result.end())
+
+
+
+#? EJERCICIO 01
+#? Encuentra la primera ocurrencia de la palabra "IA" en el siguiente texto
+#? e indica en que posición empieza y termina la coincidencia.
+text = "Todo el mundo dice que la IA nos va a quitar el trabajo. Pero solo hace falta ver cómo la puede cagar con las Regex para ir con cuidado"
+pattern = "IA"
+found_word = re.search(pattern, text)
+
+if found_word:
+    print(f"la palabra encontrada empieza en la posicion {found_word.start()} y termina en la posicion {found_word.end()}")
+else:
+    print("Palabra no encontrada")
+
